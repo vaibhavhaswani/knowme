4e513424e6712e9904e7dcfb02d2a716a9ce4101
Vaibhav Haswani <vaibhavhaswani@gmail.com>
2022-03-20 17:04:00 +0530

    expenses get api datefilter interfaced

--- knowme/fintrack/views.py
+++ knowme/fintrack/views.py
@@ -8,7 +8,8 @@ from rest_framework import status,views
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Expense
-
+from datetime import datetime
+from django.utils.timezone import make_aware #to ignore naive datetime object
 
 class ExpenseView(views.APIView):  #The viewsets base class provides the implementation for CRUD operations by default
     # serializer_class=ExpenseSerializer
@@ -21,6 +22,14 @@ class ExpenseView(views.APIView):  #The viewsets base class provides the impleme
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def get(self,request):
-        expenses=Expense.objects.all()
+        '''required date filter format : year-month-day'''
+        start=request.query_params.get('start')
+        end=request.query_params.get('end')
+        if start and end:
+            start_date=make_aware(datetime(*map(int,start.split('-'))))
+            end_date=make_aware(datetime(*map(int,end.split('-'))))
+            expenses=Expense.objects.filter(date__gte=start_date,date__lte=end_date)
+        else:
+            expenses=Expense.objects.all()
         expense_data=ExpenseSerializer(expenses,many=True)
         return Response(expense_data.data)
