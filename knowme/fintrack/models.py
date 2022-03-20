6abf55830d395564edef8b2de189cc752c3f7454
Vaibhav Haswani <vaibhavhaswani@gmail.com>
2022-03-20 18:23:47 +0530

    datefield amendments

--- knowme/fintrack/models.py
+++ knowme/fintrack/models.py
@@ -1,5 +1,8 @@
+from lib2to3.pytree import convert
 from django.db import models
 import uuid
+# from django.utils.timezone import now
+from datetime import datetime
 # Create your models here.
 
 expense_types=(
@@ -19,7 +22,7 @@ class Expense(models.Model):
     type=models.CharField(max_length=20,choices=expense_types)
     item=models.CharField(max_length=100)
     amount=models.DecimalField(max_digits=10,decimal_places=2)
-    date=models.DateTimeField(auto_now_add=True,blank=True)
+    date=models.DateField()
     important=models.BooleanField(default=False)
     
     def __str__(self):
--- knowme/fintrack/views.py
+++ knowme/fintrack/views.py
@@ -8,14 +8,14 @@ from rest_framework import status,views
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Expense
-from datetime import datetime
-from django.utils.timezone import make_aware #to ignore naive datetime object
+from datetime import date
+# from django.utils.timezone import make_aware #to ignore naive datetime object
 
 class ExpenseView(views.APIView):  #The viewsets base class provides the implementation for CRUD operations by default
     # serializer_class=ExpenseSerializer
     # queryset=Expense.objects.all()
     def post(self,request):
-        serializer=ExpenseSerializer(data=request.data)
+        serializer=ExpenseSerializer(data=request.data,many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status.HTTP_201_CREATED)
@@ -26,8 +26,8 @@ class ExpenseView(views.APIView):  #The viewsets base class provides the impleme
         start=request.query_params.get('start')
         end=request.query_params.get('end')
         if start and end:
-            start_date=make_aware(datetime(*map(int,start.split('-'))))
-            end_date=make_aware(datetime(*map(int,end.split('-'))))
+            start_date=date(*map(int,start.split('-')))
+            end_date=date(*map(int,end.split('-')))
             expenses=Expense.objects.filter(date__gte=start_date,date__lte=end_date)
         else:
             expenses=Expense.objects.all()
